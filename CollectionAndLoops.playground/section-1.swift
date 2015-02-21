9add01f746785cb6740efb887433e31521f589a1
judngu <jtnguyen.jn@gmail.com>
2015-02-21 14:13:35 -0500

    closure - shorthand argruments

--- CollectionAndLoops.playground/section-1.swift
+++ CollectionAndLoops.playground/section-1.swift
@@ -4,72 +4,72 @@
 //
 //var str = "Hello, playground"
 //
-//var data = [1, 2, 3, 4 ,5]
-//println(data)
-//
-//for i in data{
-//    
-//    println("My current dats is \(i)")
-//}
-//
-//let point = (0, 0)
-//
-////will not work because you cannot assign a new value
-////point = (1, 1)
-//
-//var mutablePoint = (9, 9)
-//// must use same data point as the one you started with
-//// mutablePoint = 4 will not work
-//
-//mutablePoint = (4, 4)
-//
-//var dict:[String: Int] = ["Jonathon": 27, "Issac": 92, "Giorgio": 74]
-//
-//for i in dict {
-//    //this returns the key and value
-//    println(i)
-//}
-//
-//for (key, value) in dict{
-//    println(value)
-//}
-//
-//var newData = [String]()
-//newData.append("one")
-////accept any kind of oblect in a newElement T -- so generic and usable you can reuse the same function over again
-////newData.append(<#newElement: T#>)
-//
+var data = [1, 2, 3, 4 ,5]
+println(data)
+
+for i in data{
+    
+    println("My current dats is \(i)")
+}
+
+let point = (0, 0)
+
+//will not work because you cannot assign a new value
+//point = (1, 1)
+
+var mutablePoint = (9, 9)
+// must use same data point as the one you started with
+// mutablePoint = 4 will not work
+
+mutablePoint = (4, 4)
+
+var dict:[String: Int] = ["Jonathon": 27, "Issac": 92, "Giorgio": 74]
+
+for i in dict {
+    //this returns the key and value
+    println(i)
+}
+
+for (key, value) in dict{
+    println(value)
+}
+
+var newData = [String]()
+newData.append("one")
+
+//accept any kind of oblect in a newElement T -- so generic and usable you can reuse the same function over again
+//newData.append(<#newElement: T#>)
 //var numData = []
-//
-//var num = "9"
-//num.toInt()
-////compiler trust me, when the code will be executed there will ve a value here -- needs to wrapped in !
-//data.append(num.toInt()!)
-//
-////range
-//for i in 0...5{
-//    
-//    println(i)
-//    
-//}
-//
-//var range = 0...5
-//
-//for i in range {
-//    println(i)
-//}
-//
-//println(data)
-////move to the third index of the array add this
-//data[2..<3] = [30, 40]
-//
-//println(data)
+
+var num = "9"
+num.toInt()
+//compiler trust me, when the code will be executed there will ve a value here -- needs to wrapped in !
+data.append(num.toInt()!)
+
+//range
+for i in 0...5{
+    
+    println(i)
+    
+}
+
+var range = 0...5
+
+for i in range {
+    println(i)
+}
+
+println(data)
+//move to the third index of the array add this
+data[2..<3] = [30, 40]
+
+println(data)
 
 
 //Create and Array with 5 [1, 2, 3, 7, 8]numbers and using a Range add to the array the missing numbers, as a bonus add the string "9: to the end of the array
-
-//var arr = [1, 2, 3, 7, 8]
-////attempt
+//
+var arr = [1, 2, 3, 7, 8]
+//attempt
 //for i in arr {
 //    if i == 1 {
 //        
@@ -83,8 +83,8 @@
 //
 //println(arr)
 //
+//
 
-//answer
 
 
 func sayHello (name:String) -> String{
@@ -101,11 +101,11 @@ func avg (numbers: Int...) ->Int {
     for i in numbers {
         sum += i
     }
-    var mean = sum/(numbers.count)
+    var mean = sum/numbers.count
     return mean
 }
 
-avg(1, 2, 3)
+avg(1, 2, 3, 5, 5)
 
 // solution using reduce method
 func avg2 (numbers: Int...) ->Int {
@@ -114,7 +114,15 @@ func avg2 (numbers: Int...) ->Int {
     return mean
 }
 
-avg2(1, 2, 3)
+avg2(1, 2, 3, 5, 5)
+
+var evens = Array(1...10).filter { (number) in number % 2 == 0 }
+println(evens)
+
+//closure - shorthand arguments
+var hellos = ["giorgio", "jon", "tom", "judy"].map({"Hello " + $0})
+
+
 
 
 
